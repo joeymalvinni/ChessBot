0749f8ef4c772bc7b26a9f2778c076c46d44d73a
Joey Malvinni <76851062+joeymalvinni@users.noreply.github.com>
2022-10-06 18:42:53 -0700

    Add functioning client code

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.cjs') || file.endsWith('.js'));
+
+const dotenv = require('dotenv');
+dotenv.config();
+const TOKEN = process.env['TOKEN'];
+
+const TEST_GUILD_ID = process.env['TEST_GUILD_ID'];
+
+const commands = [];
+
+// Creating a collection for commands in client
+client.commands = new Collection();
+
+(async () => {
+    for (const file of commandFiles) {
+        let command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
+    }
+})();
+
+// When the client is ready, this only runs once
+client.once('ready', () => {
+    console.log('Ready!');
+    // Registering the commands in the client
+    const CLIENT_ID = client.user.id;
+    const rest = new REST({
+        version: '9'
+    }).setToken(TOKEN);
+    (async () => {
+        try {
+            if (!TEST_GUILD_ID) {
+                await rest.put(
+                    Routes.applicationCommands(CLIENT_ID), {
+                        body: commands
+                    },
+                );
+                console.log('Successfully registered application commands globally');
+            } else {
+                await rest.put(
+                    Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), {
+                        body: commands
+                    },
+                );
+                console.log('Successfully registered application commands for development guild');
+            }
+        } catch (error) {
+            if (error) console.error(error);
+        }
+    })();
+});
+
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+        await command.execute(client, interaction);
+    } catch (error) {
+        if (error) console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.login(TOKEN);
